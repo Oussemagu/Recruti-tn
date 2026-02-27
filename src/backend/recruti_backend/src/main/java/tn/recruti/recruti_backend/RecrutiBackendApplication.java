26eb412e56012afaf7adc0b16c52dc3aef13a026
Oussema <guerricheoussama@gmail.com>
2026-02-27 18:06:13 +0100

    adding the springboot application architecture and boilerplate

---
+++ src/backend/recruti_backend/src/main/java/tn/recruti/recruti_backend/RecrutiBackendApplication.java
@@ -0,0 +1,13 @@
+package tn.recruti.recruti_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecrutiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecrutiBackendApplication.class, args);
+	}
+
+}
---
+++ src/backend/recruti_backend/src/test/java/tn/recruti/recruti_backend/RecrutiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.recruti.recruti_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecrutiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
